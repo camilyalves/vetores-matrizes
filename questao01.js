9d066c953537fb1a3b2e32bc0eb539968154c7d2
camilyalves <camily10.alves2007@gmail.com>
2024-04-03 16:30:25 -0400

    add questao01

---
+++ questao01.js
@@ -0,0 +1,9 @@
+/*
+Exercícios de Vetores e Matrizes em JavaScript
+Questão: Crie um vetor em JavaScript contendo os números de 1 a 5 e imprima-o
+na tela
+*/
+
+const vetor = [1, 2, 3, 4, 5];
+
+console.log(vetor);
