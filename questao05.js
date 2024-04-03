8f10712ebe1d74917278cf1db543f5802863e6a8
camilyalves <camily10.alves2007@gmail.com>
2024-04-03 17:04:17 -0400

    add questao05

---
+++ questao05.js
@@ -0,0 +1,16 @@
+/*
+Exercícios de Vetores e Matrizes em JavaScript
+Questão:  Crie uma matriz 3x3 e imprima o elemento na terceira linha e segunda
+coluna em JavaScript?
+*/
+
+const matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+  
+ 
+  const elemento = matriz[2][1];
+  console.log("Elemento na terceira linha e segunda coluna:", elemento);
+  
