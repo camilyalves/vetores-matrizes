54e2838848f2e8be35b09e3c2c600142c784e27c
camilyalves <camily10.alves2007@gmail.com>
2024-04-03 16:30:36 -0400

    add questao02

---
+++ questao02.js
@@ -0,0 +1,12 @@
+/*
+Exercícios de Vetores e Matrizes em JavaScript
+Questão: Crie um código em JavaScript para adicionar um elemento no final de
+um vetor
+*/
+let vetor = [1, 2, 3, 4, 5];
+
+let novoElemento = 6;
+
+vetor.push(novoElemento);
+
+console.log(vetor);
